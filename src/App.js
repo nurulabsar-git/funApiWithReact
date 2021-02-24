9957a3b78d8764d9f3a711e2f24f69e95d56f8c6
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-02-25 04:04:40 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,21 +6,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FirstFun name ="pakibaji"></FirstFun>
       </header>
     </div>
   );
+}
+
+function FirstFun(props){
+  const styleBtn = {
+    transitionDuration: 'transition-duration: 0.4s',
+    size: '50px',
+    borderRadius:'0.4rem',
+    color: 'gray',
+  }
+  const styleDiv ={
+    margin : '0 auto',
+    display: 'flex'
+  }
+   return (<div style ={styleDiv}>
+<input style ={{borderRadius: '0.4rem', height:'5vh', textAlign:'center'}} type="text" placeholder="Search about Bangladesh" size="50"/>
+<button  style={styleBtn}>click me</button>
+</div>
+   ) 
+}
+
+function SecondFun(props){
+  const [data, setData] = useState();
+  
 }
 
 export default App;
